a13edc26b8ff4f6e63e4b47ea60bda81c99b3ce3
Mauricio Solís <maujs2300@gmail.com>
2026-05-03 23:19:22 -0600

    Change day abbreviations to Spanish

--- src/utils/conflicts.js
+++ src/utils/conflicts.js
@@ -1,4 +1,4 @@
-export const DAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+export const DAYS = ['Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb', 'Dom'];
 export const HOURS = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22];
 
 export function doSlotsOverlap(a, b) {
